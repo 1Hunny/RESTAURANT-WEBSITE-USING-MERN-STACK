63f756a19cde4f2fc334d8d3135f825b899f46c7
HARDIK RAWAT <129970745+1Hunny@users.noreply.github.com>
2024-06-05 15:20:06 +0530

    Add files via upload

---
+++ backend/controller/reservation.js
@@ -0,0 +1,23 @@
+import ErrorHandler from "../error/error.js";
+import { Reservation } from "../models/reservationSchema.js";
+
+export const sendReservation = async (req, res, next) => {
+  const { firstName, lastName, email, date, time , phone } = req.body;
+  if (!firstName || !lastName || !email  || !date || !time || !phone) {
+    return next(new ErrorHandler("Please fill full Reservation form!", 400));
+  }
+  
+  try {
+    await Reservation.create({ firstName, lastName, email, date, time, phone });
+    res.status(201).json({
+      success: true,
+      message: "Reservation Sent Successfully!",
+    });
+  } catch (error) {
+    if (error.name === "ValidationErrors") {
+      const ValidationErrors = Object.values(error.errors).map(err => err.message);
+      return next(new ErrorHandler(ValidationErrors.join(','), 400));
+    }
+    return next(error);
+  }
+};
